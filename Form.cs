88f565825b8815f06a009f14fbae7c645d57dd72
Купалов Павел <kupalovpaul@gmail.com>
2024-06-13 13:37:31 +0700

    initial commit

---
+++ Form.cs
@@ -0,0 +1,10 @@
+namespace WindowsFormApp
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
